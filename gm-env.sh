39ed1c22dd6bfdb11384db265a5c3060ddebbae4
Andrea Grandi <a.grandi@gmail.com>
2014-06-27 16:03:33 +0100

    Organize start and stop script in a better way

--- gm-env.sh
+++ gm-env.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# Start MongoDB
-docker run -d -P --volumes-from mongodata andreagrandi/glowmachine-dev:mongo
+case "$1" in
+    start)
+        # Start MongoDB
+        echo "Starting MongoDB container..."
+        docker run --name mongo -d -P --volumes-from mongodata andreagrandi/glowmachine-dev:mongo
 
-# Start PostgreSQL
-docker run -d -P --volumes-from postgresdata andreagrandi/glowmachine-dev:postgresql
+        # Start PostgreSQL
+        echo "Starting PosgreSQL container..."
+        docker run --name postgres -d -P --volumes-from postgresdata andreagrandi/glowmachine-dev:postgresql
 
-# Start web stack
-docker run -i -P -t -v `pwd`:/glowcode andreagrandi/glowmachine-dev:web /bin/bash
+        # Start web stack
+        echo "Starting web container..."
+        docker run --name web --link mongo:mongo -i -P -t -v `pwd`:/glowcode andreagrandi/glowmachine-dev:web /bin/bash
+        ;;
+    stop)
+        echo "Stopping web container..."
+        docker stop web
+        echo "Removing web container..."
+        docker rm web
+        echo "Stopping MongoDB container..."
+        docker stop mongo
+        echo "Removing MongoDB container..."
+        docker rm mongo
+        echo "Stopping PostgreSQL container..."
+        docker stop postgres
+        echo "Stopping PostgreSQL container..."
+        docker rm postgres
+        ;;
+*)
+    echo $"Usage: $0 {start|stop}"
+    exit 1
+esac
+exit 0
